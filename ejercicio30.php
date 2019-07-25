5f10021e89ce4430744e9ed07d005a7d7ef52775
NataliaChaparro <ndchaparro5@misena.edu.co>
2019-07-25 07:50:51 +0200

    agregamos el trabajo

---
+++ ejercicio30.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Ejercicio 30</title>
+</head>
+
+<body>
+	<h1>Numeros pares</h1>
+	<?php
+	$acu=0;
+	$num=0;
+	while($acu<=29)
+	{
+		$num++;
+		
+		$par=$num%2;
+		if($par==0)
+		{
+			echo "$num </br>";
+			$acu++;
+		}
+	}
+	?>
+</body>
+</html>
